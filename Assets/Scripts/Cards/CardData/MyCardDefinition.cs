616b459ce85e6fcb37ad7f6ec4a8230895a79cff
VeraMasc <VeraMasc@gmail.com>
2024-04-03 18:53:02 +0200

    My Card Definition

---
+++ Assets/Scripts/Cards/CardData/MyCardDefinition.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using CardHouse;
+using CustomInspector;
+using UnityEngine;
+
+
+public class MyCardDefinition : CardDefinition
+{
+    public string cardName;
+
+    [TextArea(3, 10)]
+    public string cardText;
+
+    public Sprite Art;
+
+    [ShowProperty("BackArt", order=-20),Space2(16)]
+
+    [AdvancedEditorTools.Attributes.LineSeparator]
+    [SerializeReference,SubclassSelector]
+    public List<CardHelp> help;
+
+    protected void Awake() {
+        cardName = name;
+    }
+}
