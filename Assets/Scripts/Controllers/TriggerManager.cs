13d00ca50d46701d7c74dfa6be0e73083cd03489
VeraMasc <VeraMasc@gmail.com>
2024-04-18 17:20:20 +0200

    triggers

--- Assets/Scripts/Controllers/TriggerManager.cs
+++ Assets/Scripts/Controllers/TriggerManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using Unity;
 using Effect;
+using CustomInspector;
 
 
 /// <summary>
@@ -9,20 +10,61 @@ using Effect;
 [CreateAssetMenu(menuName = "Trigger/TriggerManager")]
 public class TriggerManager : ScriptableObject
 {
+    [HorizontalLine(3,message = "Exploration")]
     /// <summary>
     /// Trigger que se produce cuando una carta es revelada
     /// </summary>
     public LocalTrigger onReveal;
+    /// <summary>
+    /// Trigger que se produce al visitar una habitación (antes del combate)
+    /// </summary>
+    public LocalTrigger onVisit;
 
+    /// <summary>
+    /// Trigger que se produce tras visitar una habitación
+    /// </summary>
+    public LocalTrigger onAfterVisit;
+
+    [HorizontalLine(3,message = "Life")]
     public LocalTrigger onDeath;
     public LocalTrigger onDamage;
     public LocalTrigger onHeal;
 
     /// <summary>
+    /// Trigger producido al entrar la carta en el campo
+    /// </summary>
+    [HorizontalLine(3,message = "Board")]
+    public LocalTrigger onEnter;
+
+    /// <summary>
+    /// Se activa al destruir el objeto
+    /// </summary>
+    public LocalTrigger onDestroy;
+
+    /// <summary>
+    /// Se activa cuando el objeto deja el campo
+    /// </summary>
+    public LocalTrigger onLeave;
+
+    /// <summary>
+    /// Se activa al utilizar esta carta
+    /// </summary>
+    [HorizontalLine(3,message = "Actions")]
+    public LocalTrigger onCast;
+
+    /// <summary>
+    /// Se activa al usar CUALQUIER carta
+    /// </summary>
+    public LocalTrigger onUseAction;
+
+    /// <summary>
     /// Trigger que se produce al inicio de cada turno del jugador propietario
     /// //TODO: Crear trigger "personal" (que agrupa los objetos por jugador)
     /// </summary>
+    [HorizontalLine(3,message = "Timing")]
     public LocalTrigger onBeginTurn;
+
+    public LocalTrigger onEndTurn;
 
     //TODO: Crear trigger "regional" (que agrupa los objetos por zona)
 }
