e3b161583c40f8a5611556172b003d60a31a807b
VeraMasc <VeraMasc@gmail.com>
2024-05-02 08:39:35 +0200

    Health Display

---
+++ Assets/Scripts/Characters/UI/HealthDisplay.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+/// <summary>
+/// Gestiona el display de la salud del personaje
+/// </summary>
+public class HealthDisplay : MonoBehaviour
+{
+
+    public int health=20;
+    public int maxHealth=20;
+    public TextMeshPro textBox;
+
+    [NaughtyAttributes.Button]
+    public void refresh(){
+        if(!textBox)
+            return;
+        
+        string healthText = health.ToString();
+        if(health <=0){
+            healthText = $"<color=#330000>{healthText}</color>";
+        }else if(health > maxHealth){
+            healthText = $"<color=#f2d00d>{healthText}</color>";
+        } else if( health == maxHealth){
+            healthText = $"<color=#0ac20a>{healthText}</color>";
+        }
+
+        textBox.text = $" {healthText}<color=#800000ff>/\n<size=1>{maxHealth}</size></color>";
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
